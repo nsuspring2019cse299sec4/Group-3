330c43689bb4ee7a555e76d6f8b83eef42b928b4
mithi_shams <mithi.shams@northsouth.edu>
2019-04-09 03:13:01 +0600

    Create SplashScreen.java

---
+++ ProjectCode/app/src/main/java/com/example/khujo/SplashScreen.java
@@ -0,0 +1,51 @@
+package com.example.khujo;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ProgressBar mProgress;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        mProgress = (ProgressBar) findViewById(R.id.splash_screen_progress_bar);
+
+        mProgress.setProgressTintList(ColorStateList.valueOf(Color.WHITE));
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                doWork();
+                startApp();
+                finish();
+            }
+        }).start();
+    }
+
+    private void doWork() {
+        for (int progress=0; progress<100; progress+=10) {
+            try {
+                Thread.sleep(400);
+                mProgress.setProgress(progress);
+            } catch (Exception e) {
+                e.printStackTrace();
+                //Timber.e(e.getMessage());
+            }
+        }
+    }
+
+    private void startApp() {
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
